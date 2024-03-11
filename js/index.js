a0142d8874b7454bf5e8c546ad66fd7876d5222f
Adrian_Sguera <adrianfsguera@gmail.com>
2024-03-11 20:39:34 +0100

    11/03/2024

--- js/index.js
+++ js/index.js
@@ -8,4 +8,21 @@ window.onload = () => {
             menu.classList.add("menu-movil");
         }
     }
+
+    document.getElementById("loading").style.display="block";
+    fetch('https://pokeapi.co/api/v2/pokemon')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("loading").style.display="none";
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with your fetch operation:', error);
+        });
+
 }
