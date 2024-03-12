16a48a43d6e743392faba1b4ccd316c9f5de04a5
Adrian_Sguera <adrianfsguera@gmail.com>
2024-03-12 20:36:55 +0100

    Gradientes listos

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ window.onload = async () => {
 }
 
 async function getDataUrl(url) {
+    document.getElementById("loading").style.display = "none";
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -37,19 +38,26 @@ async function getDataUrl(url) {
 }
 
 async function loadPokemonData(pokemonData) {
-    document.getElementById("loading").style.display = "none";
     try {
-        if (!pokemon[pokemonData.name]) {
-            const response = await fetch(pokemonData.url);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            cargarInfoPkmn(data);
+        const response = await fetch(pokemonData.url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
+        const data = await response.json();
+        cargarInfoPkmn(data);
     } catch (error) {
         console.error('There was a problem with your fetch operation:', error);
     }
+}
+
+function cargarInfoPkmn(data) {
+    const types = data.types.map(typeObj => typeObj.type.name).join(", ");
+    pokemon[data.name] = {
+        img: data.sprites.front_default,
+        types: types !== "" ? types : "Unknown",
+        id: data.id,
+        exp: data.base_experience
+    };
 }
 
 function handlePaginationButtons(data) {
@@ -59,26 +67,52 @@ function handlePaginationButtons(data) {
     previous = data.previous;
 }
 
-function cargarInfoPkmn(data) {
-    pokemon[data.name] = {
-        img: data.sprites.front_default,
-        types: data.types.map(typeObj => typeObj.type.name).join(", "),
-        id: data.id,
-        exp: data.base_experience
-    };
-}
+const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD"
+};
 
-function mostrarDatosIniciales(pkmnList) {
+async function mostrarDatosIniciales(pkmnList) {
     const contenidoPkmn = pkmnList.map(pkmn => `
-        <article id="${pkmn.name}">
+        <article id="${pkmn.name}" class="pokemon-card" style="background: ${getBackgroundColor(pkmn)}">
             <h3>${pkmn.name}</h3>
-            <img src="${pokemon[pkmn.name].img}" alt="" style="width: 100px; height: 100px;">
+            <img src="${pokemon[pkmn.name].img}" alt="">
             <div>
-                <p><label>Types: </label><span>${pokemon[pkmn.name].types}</span></p>
+                <p><label>Type: </label><span>${pokemon[pkmn.name].types}</span></p>
                 <p><label>Id: </label><span>${pokemon[pkmn.name].id}</span></p>
                 <p><label>Exp: </label><span>${pokemon[pkmn.name].exp}</span></p> 
             </div>
         </article>
     `).join("");
     document.getElementById("pkmnContainer").innerHTML = contenidoPkmn;
+}
+
+function getBackgroundColor(pkmn) {
+    const types = pokemon[pkmn.name].types.split(", ");
+    if (types.length === 1) {
+        return typeColors[types[0]] || "gray"; // Si el tipo no tiene un color asignado, se usará gris
+    } else {
+        // Para tipos múltiples, calculamos un degradado entre los colores de los tipos
+        const color1 = typeColors[types[0]] || "gray";
+        const color2 = typeColors[types[1]] || "gray";
+        return `linear-gradient(to bottom right, ${color1} 30%, ${color2} 70%)`;
+        //return `linear-gradient(45deg, ${color1}, ${color2})`;
+        //return `linear-gradient(45deg, ${color1} 0%, ${color1} 50%, ${color2} 50%, ${color2} 100%)`;
+    }
 }
